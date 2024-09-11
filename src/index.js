0a538fda3941bca1855c65076c06bec3869df16b
loicmacken <loicmacken@gmail.com>
2024-09-11 14:18:07 +0200

    feat: add entrypoint with helloworld message

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
