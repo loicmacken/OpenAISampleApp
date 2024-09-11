4bdc9aca25d505cc4b5b20055f77c6d636b6ff35
loicmacken <loicmacken@gmail.com>
2024-09-11 14:35:00 +0200

    fix: incorrect variable name

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ application.get('/', (req, res) => {
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+application.listen(port, () => console.log(`Server running on port ${port}`));
