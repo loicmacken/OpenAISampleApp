cb2cce7881d9e7b155bbe5853ec44252d313f5ed
loicmacken <loicmacken@gmail.com>
2024-09-11 16:42:00 +0200

    chore: update entrypoint to use newly defined router

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import router from './routes/routes';
+
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/', router);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
