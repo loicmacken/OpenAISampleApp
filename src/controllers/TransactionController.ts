ff59dd2646178c107cfda4eeeb5f1f7b2b5db080
loicmacken <loicmacken@gmail.com>
2024-09-11 16:32:48 +0200

    feat: create transactioncontroller template (without implementation)

---
+++ src/controllers/TransactionController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+
+export const getTransactions = (req: Request, res: Response) => {
+  // TODO: Implement getTransactions
+}
+
+export const getTransactionById = (req: Request, res: Response) => {
+  // TODO: Implement getTransactionById
+}
+
+export const createTransaction = (req: Request, res: Response) => {
+  // TODO: Implement createTransaction
+}
