3f726985c4df9f42f2e1a98014a95f4b2aa9ae40
loicmacken <loicmacken@gmail.com>
2024-09-13 12:24:32 +0200

    chore: implement get transactions

--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -1,8 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
+import pool from '../models/index';
 
 export const getTransactions = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  // TODO: Implement getTransactions
+  const query = `
+    SELECT * FROM transactions;
+  `;
+  const { rows } = await pool.query(query);
+  res.status(200).json(rows);
 });
 
 export const getTransactionById = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
