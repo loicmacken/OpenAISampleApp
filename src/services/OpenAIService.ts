737c4403fb3b7f19ca9860cff17c55bbf2df3769
loicmacken <loicmacken@gmail.com>
2024-09-15 17:36:30 +0200

    chore: update openAIservice to batch requests

--- src/services/OpenAIService.ts
+++ src/services/OpenAIService.ts
@@ -1,63 +1,62 @@
 import OpenAI from 'openai';
 
-import sampleOpenAIResponse from '../controllers/sampleOpenAIResponse';
 import sampleTransactions from '../seeders/sampleTransactions';
 import validateTransaction from '../validators/TransactionValidator';
 
+const openAIClassifyTransactions = async (transactions: Array<any>) => {
+  // skip openAI API call in test and dev environment
+  if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') {
+    transactions.map((transaction: any) => {
+      transaction.transactioncategory = "Groceries";
+    });
+    return transactions;
+  }
+
+  // call openAI API to classify transactions
+  const key = process.env.OPENAI_API_KEY;
+  const client = new OpenAI({
+    apiKey: key
+  });
+  const openAIreq = {
+    model: 'gpt-4o-mini',
+    messages: transactions.map((transaction: any) => {
+      return {
+        "role": "system",
+        "content": `You have a new transaction to classify. 
+        All transactions should be classified into one of the following categories: `
+          + sampleTransactions.map((transaction: any) => transaction.transactioncategory).join(', ') + `.
+        Please classify the following transaction: ` + transaction.description,
+      };
+    }),
+    logprobs: true,
+    n: 1,
+    top_logprobs: 10
+  };
+  const openAIResponse = await client.chat.completions.create(openAIreq as any) as any;
+
+  // return transactions with category
+  openAIResponse.choices.map((choice: any, index: number) => {
+    transactions[index].transactioncategory = choice.logprobs.content[0].top_logprobs[0].token;
+  });
+
+  return transactions;
+}
+
 export const classifyTransaction = async (transaction: any) => {
   if (validateTransaction(transaction)) {
-    let openAIResponse: any;
-
-    // skip openAI API call in test and dev environment
-    if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') {
-      openAIResponse = sampleOpenAIResponse;
-    }
-    else {
-      // call openAI API to classify transaction
-      const key = process.env.OPENAI_API_KEY;
-
-      const client = new OpenAI({
-        apiKey: key
-      });
-      const openAIreq = {
-        model: 'gpt-4o-mini',
-        messages: [
-          {
-            "role": "system",
-            "content": `You have a new transaction to classify. 
-            All transactions should be classified into one of the following categories: `
-              + sampleTransactions.map((transaction: any) => transaction.transactioncategory).join(', ') + `.
-            Please classify the following transaction: ` + transaction.description,
-          }
-        ],
-        logprobs: true,
-        n: 1,
-        top_logprobs: 10
-      };
-      openAIResponse = await client.chat.completions.create(openAIreq as any) as any;
-    }
-
-    // return transaction with category
-    const category = openAIResponse.choices[0].logprobs.content[0].top_logprobs[0].token;
-    transaction.transactioncategory = category;
-    return transaction;
+    const [classifiedTransaction]: any =  openAIClassifyTransactions([transaction]);
+    return classifiedTransaction;
   }
 
   return undefined;
 }
 
 export const bulkClassifyTransactions = async (transactions: Array<any>) => {
-  let classifiedTransactions: Array<any> = [];
-
   for (const transaction of transactions) {
     if (!validateTransaction(transaction)) {
-      continue;
+      console.error("Invalid transaction data");
     }
-    let classifiedTransaction = transaction;
-    if (classifiedTransaction.transactioncategory === null) {
-      classifiedTransaction = await classifyTransaction(transaction);
-    }
-    classifiedTransactions.push(classifiedTransaction);
   }
-  return classifiedTransactions;
+
+  return await openAIClassifyTransactions(transactions);
 }
