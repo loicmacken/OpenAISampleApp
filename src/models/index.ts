62b1a918019c83bbf7c8e1ccadba6d56862c9dfe
loicmacken <loicmacken@gmail.com>
2024-09-15 18:34:25 +0200

    fix: add create table query when initializing database

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,5 @@
 import pg from 'pg';
+import Transaction from './Transaction';
 
 // Parse date and time to string to avoid timezone conversion
 pg.types.setTypeParser(pg.types.builtins.TIME, (timeStr) => timeStr);
@@ -9,5 +10,17 @@ import dbConfig from '../config/db.config';
 
 const config = dbConfig[process.env.NODE_ENV as 'development' | 'test' | 'production'];
 const pool = new pg.Pool(config as any);
+
+pool.connect().then((client) => {
+  client.query(
+    `
+        CREATE TABLE transactions (
+          ${Transaction}
+          );
+        `
+  ).then(() => {
+    client.release();
+  });
+});
 
 export default pool;
