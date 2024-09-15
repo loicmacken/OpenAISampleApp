03035c37de95c1609de9a0a962359ec40a2c24ac
loicmacken <loicmacken@gmail.com>
2024-09-15 17:53:37 +0200

    fix: incorrect db name in config

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -15,15 +15,12 @@ export default {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_TEST,
     port: process.env.DB_PORT,
-    max: 10,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
   },
   production: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_TEST,
+    database: process.env.DB_NAME_PROD,
     port: process.env.DB_PORT,
   },
 };
