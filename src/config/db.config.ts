0b6c4d172a38389afd4746cbc3e048e53c00130d
loicmacken <loicmacken@gmail.com>
2024-09-17 19:33:11 +0200

    fix: correct env variable for db host

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,21 +3,21 @@ dotenv.config();
 
 export default {
   development: {
-    host: process.env.DB_HOST_LOCAL,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_DEV,
     port: process.env.DB_PORT,
   },
   test: {
-    host: process.env.DB_HOST_LOCAL,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_TEST,
     port: process.env.DB_PORT,
   },
   production: {
-    host: process.env.DB_HOST_LOCAL,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_PROD,
