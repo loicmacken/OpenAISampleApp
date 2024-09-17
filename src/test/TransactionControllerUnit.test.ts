e64ad4fdbb9c00e90ba867151fe6ee155d305992
loicmacken <loicmacken@gmail.com>
2024-09-17 14:28:14 +0200

    test: add tests for bulkCreateTransactions

--- src/test/TransactionControllerUnit.test.ts
+++ src/test/TransactionControllerUnit.test.ts
@@ -2,7 +2,7 @@ import jest from 'jest';
 import { Request, Response } from 'express';
 import { createRequest, createResponse } from 'node-mocks-http';
 
-import { getTransactions, getTransactionById, createTransaction } from '../controllers/TransactionController';
+import { getTransactions, getTransactionById, createTransaction, bulkCreateTransactions } from '../controllers/TransactionController';
 import sampleTransactions from '../seeders/sampleTransactions';
 
 describe('Unit tests for TransactionController', () => {
@@ -77,7 +77,7 @@ describe('Unit tests for TransactionController', () => {
     await expect(res._getJSONData()).toEqual(transaction);
   });
 
-  it('should throw an error if transaction data is invalid', async () => {
+  it('should throw an error if transaction data is invalid when creating new transaction', async () => {
     const req = createRequest<Request>({
       body: {
         id: 'xxxxx',
@@ -86,6 +86,50 @@ describe('Unit tests for TransactionController', () => {
     });
     const res = createResponse<Response>();
     await createTransaction(req, res, () => {});
+    await expect(res.statusCode).toEqual(400);
+  });
+
+  it('should create multiple transactions and classify them correctly', async () => {
+    let transactions = [
+      {
+        id: 'TRN00007',
+        amount: (250.00).toFixed(2).toString(),
+        timestamp: new Date('2001-11-11').toISOString().slice(0, 19).replace('T', ' '),
+        description: 'Test transaction 7',
+        transactiontype: 'credit',
+        accountnumber: 'ACCOUN0123456789'
+      },
+      {
+        id: 'TRN00008',
+        amount: (-50.00).toFixed(2).toString(),
+        timestamp: new Date('2020-01-01').toISOString().slice(0, 19).replace('T', ' '),
+        description: 'Test transaction 8',
+        transactiontype: 'debit',
+        accountnumber: 'ACCOUN0123456789',
+        transactioncategory: 'Groceries'
+      }
+    ]
+    const req = createRequest<Request>({
+      body: transactions
+    });
+    const res = createResponse<Response>();
+    await bulkCreateTransactions(req, res, () => {});
+    await expect(res.statusCode).toEqual(201);
+    transactions[0].transactioncategory = 'Groceries';
+    await expect(res._getJSONData()).toEqual(transactions);
+  });
+
+  it('should throw an error if bulk transaction data is invalid when creating new transactions', async () => {
+    const req = createRequest<Request>({
+      body: [
+        {
+          id: 'xxxxx',
+          amount: 'xxxxx',
+        }
+      ]
+    });
+    const res = createResponse<Response>();
+    await bulkCreateTransactions(req, res, () => {});
     await expect(res.statusCode).toEqual(400);
   });
 });
