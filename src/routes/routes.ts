01058838724b0d2ea1a0c8dadb7e7f2d5b5ed43f
loicmacken <loicmacken@gmail.com>
2024-09-18 10:35:53 +0200

    feat: add homepage that lists api routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,18 @@ import { getTransactions, getTransactionById, createTransaction } from '../contr
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send(`
+    <h1>Welcome to the Transaction API</h1>
+    <p>Use the following endpoints to interact with the API:</p>
+    <ul>
+      <li>GET /transactions - get all transactions</li>
+      <li>GET /transactions/:id - get a transaction by id</li>
+      <li>POST /transactions - create a new transaction</li>
+    </ul>
+  `);
+});
+
 router.get('/transactions', getTransactions);
 router.get('/transactions/:id', getTransactionById);
 router.post('/transactions', createTransaction);
