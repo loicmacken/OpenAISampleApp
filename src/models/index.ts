d952d20381613e64394286d0057003cf619de3ef
loicmacken <loicmacken@gmail.com>
2024-09-19 14:03:36 +0200

    build: simplify to single config for database

--- src/models/index.ts
+++ src/models/index.ts
@@ -7,7 +7,7 @@ pg.types.setTypeParser(pg.types.builtins.TIMESTAMPTZ, (timeStr) => timeStr);
 
 import dbConfig from '../config/db.config';
 
-const config = dbConfig[process.env.NODE_ENV as 'development' | 'test' | 'production'];
+const config = dbConfig;
 const pool = new pg.Pool(config as any);
 
 export default pool;
