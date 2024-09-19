5e8114037aae0a72dca540d6b9dc17fea9727145
loicmacken <loicmacken@gmail.com>
2024-09-19 14:02:59 +0200

    build: simplify to single environment

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,25 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  development: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_DEV,
-    port: process.env.DB_PORT,
-  },
-  test: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_TEST,
-    port: process.env.DB_PORT,
-  },
-  production: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_PROD,
-    port: process.env.DB_PORT,
-  },
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    port: process.env.POSTGRES_PORT_LOCAL,
 };
